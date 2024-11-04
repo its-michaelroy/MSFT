00593bfcf1efde65c760ea85ca8107572528a6ce
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-11-04 15:35:13 -0500

    Initial program & files setup

---
+++ Week-1/Day-1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IntroDemoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("\nPlease enter your Age: ");
+            uint userAge = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter your height in inches: ");
+            double userHeight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nNice to meet you {userName}, youre age is {userAge} and your height is {userHeight} inches");
+          
+        }
+    }
+}
